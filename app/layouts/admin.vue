f76374327872c48a3dbe089577146018b634b0e0
schalse <schalse@redhat.com>
2026-06-24 15:24:49 +0530

    default and custom layouts

---
+++ app/layouts/admin.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h2>Admin Dashboard</h2>
+        <nav>
+            <NuxtLink to="/admin">Dashboard</NuxtLink>
+            <NuxtLink to="/">Home</NuxtLink>
+        </nav>
+        <div style="margin-top: top 20px;">
+            <slot></slot>
+        </div>
+    </div>
+</template>
---
+++ app/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <header style="background:lightblue; padding:18px ;">
+            <h2>Sit Header</h2>
+            <NuxtLink to="/">Home</NuxtLink>
+            <NuxtLink to="/admin">Admin</NuxtLink>
+        </header>
+        <main style="padding:10px">
+            <slot></slot>
+        </main>
+        <footer style="background: lightgreen; padding:18px">
+          <p>Site Footer</p>
+        </footer>
+    </div>
+</template>
