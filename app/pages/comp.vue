7dcaa1c41b62cb04107a4e1730ff98624388a4e1
schalse <schalse@redhat.com>
2026-06-25 10:16:36 +0530

    composables in nuxt

---
+++ app/pages/comp.vue
@@ -0,0 +1,12 @@
+<script setup>
+const {count,increment,decrement}=useCounter()
+</script>
+
+<template>
+    <div>
+        <h2>Counter using composables</h2>
+        <p>Count:{{ count }}</p>
+        <button @click="increment">+</button>
+        <button @click="decrement">-</button>
+    </div>
+</template>
